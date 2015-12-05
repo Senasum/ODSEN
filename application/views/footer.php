0333e9008f86ee2645bdc9b0994073dd45c53baa
Senasum <senasum@live.com>
2015-12-05 09:18:21 +0530

    begins here
    the project begins here

---
+++ application/views/footer.php
@@ -0,0 +1,15 @@
+
+    <div class="row">
+        <div class="span12">
+            
+            <div class="well">
+                <center><h3>Copyright &COPY; Sri Lanka Telecom PLC</h3></center>
+</div>
+        </div>
+     
+            
+        </div>
+
+</div>
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url("css/bootstrap.css"); ?>" />
+    </head>
+    
+     
+    <body>
+       
+        <div class="container">
+            <div class="row">
+                
+                <div class="span12">
+                    <div class="well">
+                        
+                        <center><h1>Sri Lanka Telecom PABX Unit</h1></center>
+                        
+                    </div>
+                </div>
+            </div>
+       
+    
+    
